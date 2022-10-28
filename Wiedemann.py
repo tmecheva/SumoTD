a05cb17faeef903743b0fac0319b861b09fd0eb3
tmecheva <teodora.mecheva@std.tu-plovdiv.bg>
2022-10-28 10:20:19 +0300

    28.10.22

--- Wiedemann.py
+++ Wiedemann.py
@@ -1,19 +1,14 @@
 import sys
 import TrafficDemand as TD
-import paths
-
-configFiles = ["wcfg.csv","hcfg.csv"]
-interval = [10,20,30,40]
-routingAlgorithm = ['dijkstra','astar','CH']
-
-minGap = [1,1.5,2,2.5,3]
-security = [1,2,3,4,5,6]
-estimation = [1,2,3,4,5,6]
-tau = [1,1.25]#[0.25,0.5,0.75,0.9,1,1.25]
-
-
-#command line arguments
-#cfgfile interval routingAlgorithm minGap security estimation tau
+import header
+#def CalculateWiedemann(self,cfgfile,interval,routingAlgorithm,minGap,security,estimation,tau):        
 
 td = TD.TrafficDemand()
-row = td.CalculateWiedemann(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7])
+for cfg in header.configFiles:
+    for i in header.interval:
+        for alg in header.routingAlgorithm:
+            for gap in header.minGap:
+                for s in header.security:
+                    for e in header.estimation:
+                        for t in header.tau:
+                            td.CalculateWiedemann(cfg,i,alg,gap,s,e,t)
