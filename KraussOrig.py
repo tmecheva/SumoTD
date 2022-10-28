17ee39438178aa766de3e629bb19a02dca50a660
tmecheva <teodora.mecheva@std.tu-plovdiv.bg>
2022-10-28 10:22:21 +0300

    28.10.22

--- KraussOrig.py
+++ KraussOrig.py
@@ -1,17 +1,10 @@
-import sys
 import TrafficDemand as TD
-import paths
-
-#configFiles = ["wcfg.csv","hcfg.csv"]
-#interval = [10,20,30,40]
-#routingAlgorithm = ['dijkstra','astar','CH']
-
-#minGap = [1,1.5,2,2.5,3]
-#tau = [1,1.25]#[0.25,0.5,0.75,0.9,1,1.25]
-
-
-#command line arguments
-#cfgfile interval routingAlgorithm minGap tau
+import header
 
 td = TD.TrafficDemand()
-row = td.CalculateKraussOrig(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
+for cfg in header.configFiles:
+    for i in header.interval:
+        for algo in header.routingAlgorithm:
+            for g in header.minGap:
+                for t in header.tau:
+                    td.CalculateKraussOrig(cfg,i,algo,g,t)
