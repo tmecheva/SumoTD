a84f9cafb154f561be1a2e1f074fca16ee28dbf5
tmecheva <teodora.mecheva@std.tu-plovdiv.bg>
2022-10-29 19:50:53 +0300

    29.10.22

---
+++ ControllerReports.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+import os
+import header
+from datetime import datetime
+
+class ControllerReport:
+    def __init__(self,path,junction):
+        self.junction=junction
+        self.hdf=[pd.DataFrame(columns=['Detector'])]*24
+        self.wdf=[pd.DataFrame(columns=['Detector'])]*24
+
+        self.detectors=[]
+        self.num=[]
+        self.timestamp=[]
+        self.df = pd.read_excel(os.path.join(path,junction+'.xlsx'))
+        self.row=0
+        self.df.drop([self.df.columns[1],self.df.columns[2],self.df.columns[5]], axis=1,inplace=True)
+        for x in range(0,self.df.shape[0]):
+            self.df.drop(self.df.index[x], axis=0,inplace=True)
+            if self.df.iloc[x,0]=='Дата':
+                self.df.drop(self.df.index[x], axis=0,inplace=True)
+                break
+        
+        self.Time = datetime.strptime(self.df.iloc[0,0],"%d.%m.%Y %H:%M:%S")
+
+    
+    def WriteValues(self):
+        values = pd.DataFrame(data={'Detector':self.detectors,'qPKW':self.num})
+        values.set_index('Detector',inplace=True)
+        
+        if self.Time.weekday() in [6,7]:
+            self.hdf[self.Time.hour] = pd.merge(self.hdf[self.Time.hour],values,how="outer",left_index=True,right_index=True,suffixes=("",self.Time))
+        else:
+            self.wdf[self.Time.hour] = pd.merge(self.wdf[self.Time.hour],values,how="outer",left_index=True,right_index=True,suffixes=("",self.Time))
+            
+    def GetValues(self):
+        while self.row < self.df.shape[0]:
+            currTime=datetime.strptime(self.df.iloc[self.row,0],"%d.%m.%Y %H:%M:%S")
+            if self.Time != currTime:
+                self.WriteValues()
+                self.CleanValues()
+            else:
+                self.GetValue()                
+                self.row +=1
+        
+    def CleanValues(self):
+        self.Time = datetime.strptime(self.df.iloc[self.row,0],"%d.%m.%Y %H:%M:%S")
+        self.detectors=[]
+        self.num=[]
+        self.timestamp=[]
+        
+    def WriteValuesToCSV(self,i):
+        dfH=pd.DataFrame(index=self.hdf[i].index)
+        dfW=pd.DataFrame(index=self.hdf[i].index)
+
+        dfH['Time'] = i*60
+        dfW['Time'] = i*60
+        dfH['qPKW'] = self.hdf[i].mean(axis = 1).round(0) 
+        dfW['qPKW'] = self.wdf[i].mean(axis = 1).round(0)
+        dfH['vPKW'] = 13.9
+        dfW['vPKW'] = 13.9
+        
+        dfH = dfH.dropna()
+        dfW = dfW.dropna()
+        dfH.to_csv(os.path.join(header.config_path,'hCFG.csv'),mode='a',sep=';',header=False)
+        dfW.to_csv(os.path.join(header.config_path,'wCFG.csv'),mode='a',sep=';',header=False)
+
+    def GetValue(self):        
+        det = self.df.iloc[self.row,1]
+        val = self.df.iloc[self.row,2]
+        t = self.df.iloc[self.row,0]
+        if type(det) == int and int(det)%2 == 1:
+            self.detectors.append(self.junction+':'+str(det))
+            self.num.append(val)
+
+def CreateConfigFiles():
+    dfH=pd.DataFrame(columns=['Detector','Time','qPKW','vPKW'])
+    dfW=pd.DataFrame(columns=['Detector','Time','qPKW','vPKW'])
+    dfH.to_csv(os.path.join(header.config_path,'hCFG.csv'),mode='a',sep=';',index=False)
+    dfW.to_csv(os.path.join(header.config_path,'wCFG.csv'),mode='a',sep=';',index=False)
+        
+    CRList = []
+    for j in header.junctions:
+        CRList.append(ControllerReport(header.reports_path,j))
+                      
+    for cr in CRList:
+        cr.GetValues()
+        
+    for i in range(0,23):
+        for cr in CRList:
+            cr.WriteValuesToCSV(i)
+            
+CreateConfigFiles()
+        
+                      
+    
--- paths.py
+++
@@ -1,8 +0,0 @@
-simulation_path = "/home/tedy/Sumo/myscript/simulation/"
-network_path=simulation_path+'updated.net.xml'
-sumo_tools_path = "/usr/share/sumo/tools/"
-data_path = "/home/tedy/Sumo/Data/"
-out_path= "/home/tedy/Sumo/Data/Out/"
-result_path = data_path+"Result"
-config_path = "/home/tedy/Sumo/myscript/config/"
-vtype = " -y 'vtype=\"vdist1\"'"
